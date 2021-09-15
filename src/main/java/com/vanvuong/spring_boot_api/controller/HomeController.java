58187c16132e3f4f4758115f5e6adfdb2d763592
VanVuong <lai.vanvuong2866@gmail.com>
2021-09-15 11:12:23 +0700

    add home controller

---
+++ src/main/java/com/vanvuong/spring_boot_api/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.vanvuong.spring_boot_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@GetMapping("/")
+	public String home() {
+		return "Lai Van Vuon - 18072661";
+	}
+}
