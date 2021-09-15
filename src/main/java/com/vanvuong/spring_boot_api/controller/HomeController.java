408f98152f335853ee577fe0922a8f4c65fecbda
VanVuong-Github <74919638+VanVuong-Github@users.noreply.github.com>
2021-09-15 11:48:03 +0700

    Update HomeController.java

--- src/main/java/com/vanvuong/spring_boot_api/controller/HomeController.java
+++ src/main/java/com/vanvuong/spring_boot_api/controller/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 	@GetMapping("/")
 	public String home() {
-		return "Lai Van Vuon - 18072661";
+		return "Lai Van Vuong - 18072661";
 	}
 }
